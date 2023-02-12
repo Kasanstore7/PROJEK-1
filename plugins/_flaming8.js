76bbac06d6c549e2ac3484db4f9de26c6434dbdc
Mshunone <100399123+Mshunone@users.noreply.github.com>
2023-02-12 21:45:52 +0700

    Update _flaming8.js

--- plugins/_flaming8.js
+++ plugins/_flaming8.js
@@ -1,10 +1,10 @@
 var fetch = require('node-fetch')
 var handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Text\nContoh : .shadowsky BOTCAHX'
+  if (!args[0]) throw 'Masukkan Text\nContoh : .shadowsky SAXIA'
   m.reply('_Proses..._')
   var res = `https://api.botcahx.biz.id/api/photooxy/shadow-sky?text=${response[0]}&apikey=Admin`
-  conn.sendFile(m.chat, res, 'botcahx.jpg', `© BOTCAHX`, m, false)
+  conn.sendFile(m.chat, res, 'Saxia.jpg', `「 http://ẉ˘Saꭙࣼia.id 」`, m, false)
 }
 handler.help = ['shadowsky'].map(v => v + ' <text>')
 handler.tags = ['photooxy']
