2f0cdb196112b6d3ed4b5bf419563fb415b06611
Mshunone <100399123+Mshunone@users.noreply.github.com>
2023-02-13 01:23:34 +0700

    Update main-thanks.js

--- plugins/main-thanks.js
+++ plugins/main-thanks.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://database.tioclkp02.repl.co/1240fb4a4d600fb02355b.png', m, { packname: "Sama Sama by", author: "Tio" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/31a705379b764fa20c5e5.png', m, { packname: "wa.me/6283111136642", author: "Saxia Botz\nig @m.rxcl_" })
 }
 
-handler.customPrefix = /^(Terima Kasih|Thanks|tq)$/i
+handler.customPrefix = /^(Terima Kasih|Thanks|tq|makasih|makasih|makaci|thx)$/i
 handler.command = new RegExp
 
 module.exports = handler
