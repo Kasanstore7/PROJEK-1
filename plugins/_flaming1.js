fff9f7581aeaed954a6acda74868ab05fcc069fc
Mshunone <100399123+Mshunone@users.noreply.github.com>
2023-02-12 21:38:49 +0700

    Update _flaming1.js

--- plugins/_flaming1.js
+++ plugins/_flaming1.js
@@ -4,10 +4,10 @@ conn,
 args 
 }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Text\nContoh : .flaming1 BOTCAHX'
+  if (!args[0]) throw 'Masukkan Text\nContoh : .flaming1 SAXIA'
   m.reply('_Proses..._')
   var res = `https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&script=fluffy-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=${response[0]}`
-  conn.sendFile(m.chat, res, 'gura.jpg', `© BOTCAHX`, m, false)
+  conn.sendFile(m.chat, res, 'Saxia.jpg', `「 http://ẉ˘Saꭙࣼia.id 」`, m, false)
 }
 handler.help = ['flaming1'].map(v => v + ' <text>')
 handler.tags = ['maker']
