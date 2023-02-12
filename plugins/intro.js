56dcfe7397c0f8d1a3c651583c8304368ec1ad49
Mshunone <100399123+Mshunone@users.noreply.github.com>
2023-02-13 00:38:30 +0700

    Update intro.js

--- plugins/intro.js
+++ plugins/intro.js
@@ -2,18 +2,19 @@ let handler = async m => {
 
 let krtu = `Kartu Intro`
 m.reply(`
-╭─── *「 Kartu Intro 」*
-│       
-│ *Nama     :* 
-│ *Gender   :* 
-│ *Umur      :* 
-│ *Hobby    :* 
-│ *Kelas      :* 
-│ *Asal         :* 
-│ *Agama    :* 
-│ *Status     :* 
-╰──────────────
-`.trim()) 
+0ཻུ۪۪ꦽꦼ̷⸙‹•───────────⬣
+│     *「 TEKS INTRO 」*
+│
+│ • *Nama     :*
+│ • *Gender   :* 
+│ • *Umur      :* 
+│ • *Hobby    :* 
+│ • *Kelas      :* 
+│ • *Asal        :* 
+│
+└──ꪶ ཻུ۪۪ꦽꦼ̷⸙ ━ ━ ━ ━ ꪶ ཻུ۪۪ꦽꦼ̷⸙⬣
+「 ẉ_Saꭙࣼia˘Botz.id 」
+`.trim()) // Tambah sendiri kalo mau
 }
 handler.command = /^(intro)$/i
 
