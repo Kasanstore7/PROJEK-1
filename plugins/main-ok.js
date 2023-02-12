9da6a10fd877bc453e588817a0b1d23f2c93dd99
Mshunone <100399123+Mshunone@users.noreply.github.com>
2023-02-13 01:22:15 +0700

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://database.tioclkp02.repl.co/f61a7c96f3f86b6c5214f.png', m, { packname: "Database Replit", author: "replit @tioclkp02 500gb Room\n\n> No enc, Private Content" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f0b1c19aca4215ffb4ac7.jpg', m, { packname: "wa.me/6283111136642", author: "Saxia Botz\nig @m.rxcl_" })
 }
 
-handler.customPrefix = /^(ok)$/i
+handler.customPrefix = /^(ok|okey|otey|okeh|oke)$/i
 handler.command = new RegExp
 
 module.exports = handler
