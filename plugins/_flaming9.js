331bfb54acf43bde4ef73196c753de8120320600
Mshunone <100399123+Mshunone@users.noreply.github.com>
2023-02-12 21:46:17 +0700

    Update _flaming9.js

--- plugins/_flaming9.js
+++ plugins/_flaming9.js
@@ -1,10 +1,10 @@
 var fetch = require('node-fetch')
 var handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Text\nContoh : .naruto BOTCAHX'
+  if (!args[0]) throw 'Masukkan Text\nContoh : .naruto SAXIA'
   m.reply('_Proses..._')
   var res = `https://api.botcahx.biz.id/api/photooxy/naruto?text=${response[0]}&apikey=Admin`
-  conn.sendFile(m.chat, res, 'botcahx.jpg', `© BOTCAHX`, m, false)
+  conn.sendFile(m.chat, res, 'Saxia.jpg', `「 http://ẉ˘Saꭙࣼia.id 」`, m, false)
 }
 handler.help = ['naruto'].map(v => v + ' <text>')
 handler.tags = ['maker']
