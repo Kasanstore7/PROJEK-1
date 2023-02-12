4ec7da7bc3e9e4029c93476fd8068df4f3053df9
Mshunone <100399123+Mshunone@users.noreply.github.com>
2023-02-12 21:39:22 +0700

    Update _flaming11.js

--- plugins/_flaming11.js
+++ plugins/_flaming11.js
@@ -4,10 +4,10 @@ conn,
 args 
 }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Text\nContoh : .grass BOTCAHX'
+  if (!args[0]) throw 'Masukkan Text\nContoh : .grass SAXIA'
   m.reply('_Proses..._')
   var res = `https://api.botcahx.biz.id/api/photooxy/under-grass?text=${response[0]}&apikey=Admin`
-  conn.sendFile(m.chat, res, 'botcahx.jpg', `© BOTCAHX`, m, false)
+  conn.sendFile(m.chat, res, 'Saxia.jpg', `「 http://ẉ˘Saꭙࣼia.id 」`, m, false)
 }
 handler.help = ['grass'].map(v => v + ' <text>')
 handler.tags = ['photooxy']
