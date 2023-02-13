621fd175f85b30862cf39e602db99f17e963210a
Mshunone <100399123+Mshunone@users.noreply.github.com>
2023-02-13 07:10:56 +0700

    Update _anti+212.js

--- plugins/_anti+212.js
+++ plugins/_anti+212.js
@@ -4,7 +4,7 @@ handler.before = async function (m) {
    if (m.sender.startsWith('212' || '212')) {
    	global.db.data.users[m.sender].banned = true
    }
-   
+ }
    if (m.sender.startsWith('265' || '265')) {
    	global.db.data.users[m.sender].banned = true
    }
